461cb74f5639f41ad8881eee2e3c5f601c4861e9
romul0 <49355445+romul0@users.noreply.github.com>
2019-06-08 11:52:54 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,32 +1,50 @@
+// gcc -o Jogo Jogo.c -lSDL2 -lSDL2_image -lSDL2_mixer  <-- compilar
+
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdbool.h>
+#include <SDL2/SDL_mixer.h>
+
+#define LARGURA 3200
+#define ALTURA 1800
+#define PLAYER_w 31 // Largura da sprite
+#define PLAYER_h 46 // Altura da sprite
 
 SDL_Renderer* render;
 SDL_Event event;
 SDL_Window* Janela = NULL;
 SDL_Surface* PlayerSurface;
+SDL_Surface* Background = NULL;
 SDL_Texture* PlayerTexture;
+Mix_Chunk* Musica;
+SDL_Surface* Background;
+SDL_Texture* Textura_Fundo;
+//SDL_Surface *surface
 
-SDL_Rect sRect = {0, 0, 40, 47};
-
-SDL_Rect dRect = {520, 300, 76, 96};
+SDL_Rect sPlayer = {0, 0, PLAYER_w, PLAYER_h}; //Sprites Player
+SDL_Rect dPlayer = {640, 360, 75, 90}; //Movimentação Player
+SDL_Rect sMapa = {0, 0, 320, 180}; //Movimento do mapa
 
 bool esquerda = false, direita = false, cima = false, baixo = false;
 
-bool Inicio ();
+bool Inicio();
+bool Render_Janela ();
+bool Render();
+bool XPlayer();
+void Andar_Tecla();
+void Andar_Logic();
+void Musicas_Tops();
+//bool Colidir_Janela();
+void Obter_Fundo ();
+void Rolamento_Tela();
 
-bool Render ();
- 
-bool XPlayer ();
-
-void Andar ();
 
 int main (){
 
-	//Início//
 	SDL_Init (SDL_INIT_EVERYTHING);
+
+	Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 4096);
 
 	bool Play = true;
 
@@ -36,12 +54,14 @@ int main (){
 
 	}
 	else{
-
+		Render_Janela ();
+		Musicas_Tops();
 		const int FPS = 20;                    // // // // // //
 		const int FrameDelay = 1000/FPS;      //  Frame Per  //
 		unsigned long FrameStart;            //    Second   //
 		int FrameTime;                      // // // // // //
 		XPlayer();
+		Obter_Fundo();
 
 			while (Play){
 
@@ -49,12 +69,15 @@ int main (){
 				Render();
 
 				while(SDL_PollEvent(&event)){
-					//X para fechar
-					if (event.type == SDL_QUIT){
+
+					if (event.type == SDL_QUIT){ //Botão X para fechar
 						Play = false;
 					}
 
-					Andar();
+					Andar_Tecla ();
+					//Colidir_Janela();
+					Rolamento_Tela();
+					Andar_Logic ();
 
 				}
 			FrameTime = SDL_GetTicks() - FrameStart;
@@ -64,11 +87,17 @@ int main (){
 			}
 			}	
 	}
+	Mix_FreeChunk(Musica);
+	SDL_FreeSurface(PlayerSurface);
+    SDL_FreeSurface(Background);
+
     //Destruindo geral
+    Mix_CloseAudio();
     SDL_DestroyWindow(Janela);
     SDL_DestroyRenderer(render); 
     SDL_DestroyTexture(PlayerTexture);
-    SDL_FreeSurface(PlayerSurface);
+    SDL_DestroyTexture(Textura_Fundo);
+
 
     //Fim//
     SDL_Quit();
@@ -81,45 +110,68 @@ bool Inicio (){
 
 	//Inicializando SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0){
-		printf( "ERRO! A tela não pode ser inicializada. Erro: %s\n", SDL_GetError());
+		printf( "ERRO! A tela não pode ser inicializada.");
 		success = false;
 	}
-	else {
-		Janela = SDL_CreateWindow ("Jogo 100% boladaço", SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_RESIZABLE);
+	
+	else { Janela = SDL_CreateWindow ("Jogo 100% boladaço", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_RESIZABLE); }
 
-		if (Janela == NULL){
-			printf ("A janela não pôde ser criada! Erro: %s\n", SDL_GetError());
-			success = false;
-	}
-		else {
-			//Obtendo render da janela
-			render = SDL_CreateRenderer(Janela, -1, 0);
-
-			SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
-		}
-	}
 	return success;
 }
 
-bool Render (void){
+bool Render_Janela (void){
 
-	SDL_RenderCopy(render, PlayerTexture, &(sRect), &(dRect));
+	render = SDL_CreateRenderer(Janela, -1, 0); //Obtendo render da janela
+	SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
+}
 
-	SDL_RenderPresent(render);
+void Obter_Fundo (void){ //Imagem de fundo
+
+	Background = IMG_Load("Mapa.png");
+	Textura_Fundo = SDL_CreateTextureFromSurface (render, Background);
+}
+
+bool Render (void){ //Precisa de Render Copy para tudo que for ser exibido na tela
 
 	SDL_RenderClear(render);
+	SDL_RenderCopy (render, Textura_Fundo, &sMapa, NULL); // Onde será apresentado, textura do que será apresentado, posição, posição
+	SDL_RenderCopy(render, PlayerTexture, &(sPlayer), &(dPlayer));
+	SDL_RenderPresent(render);
 }
 
 bool XPlayer (void){
 
-	PlayerSurface = IMG_Load("Sprites/Morte.png");
-
+	PlayerSurface = IMG_Load("Sprites/Player.png");
 	PlayerTexture = SDL_CreateTextureFromSurface(render, PlayerSurface);
 }
 
-void Andar(){
-	if (event.type == SDL_KEYDOWN){
+void Musicas_Tops(){
+
+	Musica = Mix_LoadWAV("Melodia/URSS.mp3");
+	Mix_AllocateChannels(8); //Alocar canais para as músicas
+	Mix_Volume(1, 8); //Canal e volume do canal, o volume vai de 0 a 10
+	Mix_PlayChannel(1, Musica, -1); //Canal que vai tocar, musica que será tocada, quantidade de vezes que será tocada
+}
+
+/*bool Colidir_Janela (void){ 	//Colisão com Janela      
+     
+    if(dPlayer.y + dPlayer.h >= ALTURA){
+        baixo = false;
+    }
+    if(dPlayer.y <= 0){
+    	cima = false;
+    }
+    if(dPlayer.x + dPlayer.w >= LARGURA){
+    	direita = false;
+    }
+    if(dPlayer.x  <= 0){
+    	esquerda = false;
+    }
+}*/
+
+void Andar_Tecla(){
+	
+	if (event.type == SDL_KEYDOWN){ //Usuário pressionou  uma tecla
 
 		if (event.key.keysym.sym == SDLK_UP)
 			cima = true;
@@ -130,8 +182,8 @@ void Andar(){
 		if (event.key.keysym.sym == SDLK_RIGHT)
 			direita = true;	
 	}
-	//Usuário parou de pressionar a tecla
-	else if (event.type == SDL_KEYUP){
+	
+	else if (event.type == SDL_KEYUP){ //Usuário parou de pressionar a tecla
 
 		if (event.key.keysym.sym == SDLK_UP)
 			cima = false;
@@ -142,51 +194,77 @@ void Andar(){
 		if (event.key.keysym.sym == SDLK_RIGHT)
 			direita = false;
 	}
-	//Parte de Lógica
-	if (esquerda == true && direita == false && cima == false && baixo == false){
-		dRect.x -= 4;
-		sRect.y = 47;
-		if(sRect.x < 120){
-			sRect.x += 40;
-		}
-		else{sRect.x = 0;}
-		}
+}
 
-	if (direita == true && esquerda == false && cima == false && baixo == false){
-		dRect.x += 4;
-		sRect.y = 94;
-		if(sRect.x < 120){
-			sRect.x += 40;
-		}
-		else{sRect.x = 0;}
-		}
+void Andar_Logic(){	//Parte de Lógica
 
-	if (cima == true && baixo == false && esquerda == false && direita == false){
-		dRect.y -= 4;
-		sRect.y = 141;
-		if(sRect.x < 120){
-			sRect.x += 40;
+	if (baixo == true && direita == false && esquerda == false){
+		sPlayer.y = PLAYER_h * 0;
+		if(sPlayer.x < PLAYER_w * 3){
+			sPlayer.x += PLAYER_w;
 		}
-		else{sRect.x = 0;}
+		else{sPlayer.x = 0;}
 	}
 
-	if (baixo == true && cima == false && esquerda == false && direita == false){
-			dRect.y += 4;
-			sRect.y = 0;
-		if(sRect.x < 120){
-			sRect.x += 40;
+	if (cima == true && direita == false && esquerda == false){
+		sPlayer.y = PLAYER_h;
+		if(sPlayer.x < PLAYER_w * 3){
+			sPlayer.x += PLAYER_w;
 		}
-		else{sRect.x = 0;}
+		else{sPlayer.x = 0;}
 	}
 
-	if ((cima == true && baixo == true) || (esquerda == true && direita == true) ||
-		(cima == true && esquerda == true) || (cima == true && direita == true) ||
-		(baixo == true && esquerda == true) || (baixo == true && direita == true)){
-
-			sRect.x = 0;
+	if (esquerda == true && cima == false && baixo == false){
+		sPlayer.y = PLAYER_h * 2;
+		if(sPlayer.x < PLAYER_w * 3){
+			sPlayer.x += PLAYER_w;
+		}
+		else{sPlayer.x = 0;}
 	}
 
-		if(cima == false && baixo == false && esquerda == false && direita == false){
-			sRect.x = 0;
+	if (direita == true && cima == false && baixo == false){
+		sPlayer.y = PLAYER_h * 3;
+		if(sPlayer.x < PLAYER_w * 3){
+			sPlayer.x += PLAYER_w;
+		}
+		else{sPlayer.x = 0;}
 	}
+
+	if(cima == false && baixo == false && esquerda == false && direita == false)
+		sPlayer.x = 0;
+
+	if ((cima == true && baixo == true) || (esquerda == true && direita == true)){ 
+		sPlayer.x = 0;
+	}
+}
+
+void Rolamento_Tela(){
+
+	if (baixo == true && direita == false && esquerda == false){
+		if(sMapa.y + sMapa.h + 2 <= ALTURA) // colisão da tela
+			sMapa.y += 2; // O mapa anda e o player fica parado, so movimenta a sprite
+		else
+			dPlayer.y += 2; //Quando o mapa chega no fim o player anda
+	}
+
+	if (cima == true && direita == false && esquerda == false){
+		if(sMapa.y - 2 >= 0)
+			sMapa.y -= 2;
+		else
+			dPlayer.y -= 2;
+	}
+
+	if (direita == true && cima == false && baixo == false){
+		if(sMapa.x + sMapa.w + 2 <= LARGURA)
+			sMapa.x += 2;
+		else
+			dPlayer.x += 2;
+	}
+
+	if (esquerda == true && cima == false && baixo == false){
+		if(sMapa.x - 2 >= 0)
+			sMapa.x -= 2;
+		else
+			dPlayer.x -= 2;
+	}	
 }
